6102e8ac44f31a89971ae3a3aae86b670f5a2874
Jonathan Clarke <jonathan@beilabs.com>
2013-04-18 18:46:00 +1000

    Adding base rails 4 beta 1 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fola::Application.config.secret_key_base = '8b18ff95b9a9906b40ce737f92d0c253ab16fafa923d07a69c50c6b8784742e19ad2377040136bc8cf14217ab52206b8a27e899218d032f10ad7b9df84da895e'
